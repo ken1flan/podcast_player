9657bdcf7a73e1b2fddb26efff4ec2891923f02f
ken1flan <ken1flan@gmail.com>
2025-05-04 22:27:15 +0900

    fix

--- test/helpers/test_sized_box.dart
+++ test/helpers/test_sized_box.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 
 class TestSizedBox extends StatelessWidget {
-  final Widget child;
-
+  // ウィジェットのときにはkeyを最初に書くのが一般的なため
+  // ignore: always_put_required_named_parameters_first
   const TestSizedBox({super.key, required this.child});
+  final Widget child;
 
   @override
   Widget build(BuildContext context) {
