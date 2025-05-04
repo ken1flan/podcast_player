7be34f6d33cfbc65a1a77ae67f8eb584f2b22385
ken1flan <ken1flan@gmail.com>
2025-05-04 10:13:14 +0900

    MyAppを動かしたときの画面を保存

--- test/golden/my_app_test.dart
+++ test/golden/my_app_test.dart
@@ -1,21 +1,22 @@
 import 'package:alchemist/alchemist.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-// import 'package:podcast_player/my_app.dart';
+import 'package:podcast_player/my_app.dart';
 
 void main() {
   goldenTest(
-    'ElevatedButton renders tap indicator when pressed',
-    fileName: 'elevated_button_pressed',
-    whilePerforming: press(find.byType(ElevatedButton)),
+    'Run my_app',
+    fileName: 'my_app',
     builder:
         () => GoldenTestGroup(
           children: [
             GoldenTestScenario(
-              name: 'pressed',
-              child: ElevatedButton(
-                onPressed: () {},
-                child: const Text('Pressed'),
+              name: 'my_app',
+              child: ConstrainedBox(
+                constraints: const BoxConstraints(
+                  maxWidth: 300,
+                  maxHeight: 600,
+                ),
+                child: const MyApp(),
               ),
             ),
           ],
