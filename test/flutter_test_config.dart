0d66105e0f350bd0192c8b72e3e368976013829e
ken1flan <ken1flan@gmail.com>
2025-05-05 11:22:00 +0900

    CIのテストで差分を出にくい設定に変更
    - フォントを■
    - 影をなし
    
    これによって、デザインの差異の検出がざっくりになってしまいますが…
    環境やバージョンで差分が出にくいそうです。
    
    このプロジェクトは、そんなにデザイン重視ではないので、
    これでいきます。

--- test/flutter_test_config.dart
+++ test/flutter_test_config.dart
@@ -4,7 +4,6 @@ import 'package:alchemist/alchemist.dart';
 Future<void> testExecutable(FutureOr<void> Function() testMain) async {
   return AlchemistConfig.runWithConfig(
     config: const AlchemistConfig(
-      ciGoldensConfig: CiGoldensConfig(obscureText: false, renderShadows: true),
       platformGoldensConfig: PlatformGoldensConfig(enabled: false),
     ),
     run: testMain,
